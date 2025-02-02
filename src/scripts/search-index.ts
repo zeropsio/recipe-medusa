77b75a4cd525a95014303cceac05bfea34b959d2
Ales Rechtorik <foxxcz@gmail.com>
2025-02-02 15:19:13 +0100

    search index script

---
+++ src/scripts/search-index.ts
@@ -0,0 +1,22 @@
+import {
+  ExecArgs,
+  IProductModuleService,
+} from "@medusajs/framework/types"
+import { Modules, SearchUtils } from "@medusajs/framework/utils"
+import { MeiliSearchService } from '@rokmohar/medusa-plugin-meilisearch'
+
+export default async function searchIndexScript({ container }: ExecArgs) {
+  const productModuleService: IProductModuleService = container.resolve(
+    Modules.PRODUCT
+  )
+
+  const meilisearchIndexService: MeiliSearchService = container.resolve(
+    'index-meilisearch'
+  )
+
+  const products = await productModuleService.listProducts()
+
+  const res = await meilisearchIndexService.addDocuments('products', products, SearchUtils.indexTypes.PRODUCTS)
+
+  console.log({ products, res })
+}
